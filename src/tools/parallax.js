d2cb67c20a9e38bea11894ec1493a790792a9fad
DESKTOP-GSNJ48I\user008 <kumo@percentage.asia>
2023-02-15 18:57:14 +0800

    0215

--- src/tools/parallax.js
+++ src/tools/parallax.js
@@ -11,14 +11,14 @@ export default {
       scrollImg.each(function(){
         let posTop = $(this).position().top;
         let imgH = $(this).outerHeight();
-        let img = $(this).find('img');
+        let img = $(this).find('[data-scroll-obj]');
         // console.log(posTop);
         if(scrollAfter > (posTop / 2)) {
           // console.log('in');
-          img.css('transform', `translateY(-${scrollAfter * 0.3}px)`);
+          img.css('transform', `translateY(-${(scrollAfter - imgH) * 0.25}px)`);
           if(scrollAfter > (posTop + (imgH / 2))) {
             // console.log('out');
-            img.css('transform', `translateY(-${(posTop + (imgH / 2)) * 0.3}px)`);
+            img.css('transform', `translateY(-${(posTop + (imgH / 2)) * 0.25}px)`);
           }
         }
       })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,12 +8,12 @@ onMounted(() => {
 </script>
 
 <template>
-  <div>
+  <div style="height: 3000px">
     <!-- banner -->
     <section class="section-banner flex items-center h-screen">
       <div class="scroll-wrap">
         <div class="scroll-container">
-          <img src="@/assets/images/4.jpeg" alt="">
+          <img src="@/assets/images/4.jpeg" alt="" data-scroll-obj>
         </div>
       </div>
       <h2 class="font-Playfair z-[1] m-[-30px]">
@@ -23,10 +23,48 @@ onMounted(() => {
       </h2>
     </section>
     <!-- section 1 -->
-    <section class="section1">
+    <section class="section1 pb-10">
       <div class="scroll-wrap">
         <div class="scroll-container">
-          <img src="@/assets/images/4.jpeg" alt="">
+          <img src="@/assets/images/bg1.jpg" alt="" data-scroll-obj>
+        </div>
+      </div>
+    </section>
+    <!-- section 2 -->
+    <section class="section2">
+      <h6>ABOUT BCST</h6>
+      <div class="">
+        <div class="block-item flex">
+          <div class="scroll-wrap">
+            <div class="scroll-container">
+              <img src="@/assets/images/9.jpg" alt="" data-scroll-obj>
+            </div>
+          </div>
+          <div class="text">
+            <h3></h3>
+            <p></p>
+          </div>
+        </div>
+        <div class="block-item flex flex-row-reverse">
+          <div class="scroll-wrap">
+            <div class="scroll-container">
+              <img src="@/assets/images/8.jpg" alt="" data-scroll-obj>
+            </div>
+          </div>
+          <div class="text">
+            <h3></h3>
+            <p></p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- section 3 -->
+    <section>
+      <h6>STORY</h6>
+      <div class="row4">
+        <div class="block-item">
+          <div class="photo"></div>
+
         </div>
       </div>
     </section>
@@ -40,11 +78,40 @@ onMounted(() => {
     .scroll-wrap {
       max-width: 1120px;
     }
+    .scroll-container {
+      padding-bottom: 66.6%;
+      img {
+        height: 125%;
+        width: auto;
+      }
+    }
   }
 
   .section1 {
     .scroll-wrap {
       max-width: 1440px;
+      width: 90%;
+      margin: 0 auto;
+    }
+    .scroll-container{
+      padding-bottom: 23%;
+      img {
+        height: auto;
+        width: 100%;
+      }
+    }
+  }
+
+  .section2 {
+    .scroll-wrap {
+      max-width: 600px;
+    }
+    .scroll-container {
+      padding-bottom: 110%;
+      img {
+        height: 100%;
+        width: auto;
+      }
     }
   }
 </style>
